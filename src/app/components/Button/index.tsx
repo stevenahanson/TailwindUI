f5cc6e54faaff12d919d65f98e9de4e46239f1c1
Steven Hanson <stevenahanson@live.com>
2023-10-21 16:22:38 +0100

    Move button to its own component

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,3 +1,11 @@
 export default function Button() {
-  return "something";
+  return (
+    <>
+      <div className="w-full h-full flex items-center justify-center absolute">
+        <a href="#" className="bg-blue-400 text-white text-5xl p-5">
+          Button
+        </a>
+      </div>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import Button from "./components/Button";
 
 export default function Home() {
-  return (
-    <>
-      <div className="w-full h-full flex items-center justify-center absolute">
-        <a href="#" className="bg-orange-400 text-white text-5xl p-5">
-          Button
-        </a>
-      </div>
-      <Button />
-    </>
-  );
+  return <Button />;
 }
