1da4fc1267e2cb0f870832a1465b21244b25a805
Steven Hanson <stevenahanson@live.com>
2023-10-21 16:18:01 +0100

    Style button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-full flex items-center justify-center absolute">
-      <a href="#" className="bg-orange-400">
+      <a href="#" className="bg-orange-400 text-white text-5xl p-5">
         Button
       </a>
     </div>
