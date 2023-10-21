8653ed7c7d0e4b7b71b4c461189e7f27b8ea77c3
Steven Hanson <stevenahanson@live.com>
2023-10-21 16:20:30 +0100

    Render new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Button from "./components/Button";
+
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center absolute">
-      <a href="#" className="bg-orange-400 text-white text-5xl p-5">
-        Button
-      </a>
-    </div>
+    <>
+      <div className="w-full h-full flex items-center justify-center absolute">
+        <a href="#" className="bg-orange-400 text-white text-5xl p-5">
+          Button
+        </a>
+      </div>
+      <Button />
+    </>
   );
 }
