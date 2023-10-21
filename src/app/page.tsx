bc9b29ef6a9c3137ad091c7a287fc56fe876f7d5
Steven Hanson <stevenahanson@live.com>
2023-10-21 16:13:43 +0100

    Move button to center of screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center absolute">
       <a href="#" className="bg-orange-400">
         Button
       </a>
