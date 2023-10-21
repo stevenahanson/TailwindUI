8ce9ad2363dd042514ab8541966b6bc1525c4908
Steven Hanson <stevenahanson@live.com>
2023-10-21 16:26:56 +0100

    Add console log on button click

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 export default function Button() {
   return (
     <>
       <div className="w-full h-full flex items-center justify-center absolute">
-        <a href="#" className="bg-blue-400 text-white text-5xl p-5">
+        <a
+          href="#"
+          className="bg-blue-400 text-white text-5xl p-5"
+          onClick={() => console.log("clicked!")}
+        >
           Button
         </a>
       </div>
