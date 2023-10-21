1d7f6e00ac74521b97c8f6d6b8f11a7f75ac31f7
Steven Hanson <stevenahanson@live.com>
2023-10-21 16:30:24 +0100

    Hide button on click

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Button() {
+  const [showButton, setShowButton] = useState(true);
+
   return (
     <>
       <div className="w-full h-full flex items-center justify-center absolute">
         <a
           href="#"
-          className="bg-blue-400 text-white text-5xl p-5"
-          onClick={() => console.log("clicked!")}
+          className={`bg-blue-400 text-white text-5xl p-5 ${
+            showButton ? "" : "hidden"
+          }`}
+          onClick={() => setShowButton(false)}
         >
           Button
         </a>
